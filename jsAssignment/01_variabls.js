2208d1e6f2a62ff0b3815439f6ae9cd57bf42478
bhushandakhare <bhushandakhare20@gmail.com>
2023-09-20 11:19:49 +0530

    variable assignment
    variable assignment

---
+++ jsAssignment/01_variabls.js
@@ -0,0 +1,34 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName;
+var myFullName="Bhushan Dakhare";
+console.log(myFullName);
+
+myFullName="Bhushan Gajanan Dakhare";
+console.log(myFullName);
+
+var love="I LOVE Js";
+console.log(love);
+
+var firstName="firstName: Bhushan";
+console.log(firstName);
+
+var lastName="lastName: Dakhare";
+console.log(lastName);
+
+var age="age: 25";9
+console.log(age); 
+
+var address="Chandrapur";
+console.log(address);
+
+var pinCode="442917";
+console.log(pinCode);
+
+var gender="Male";
+console.log(gender);
+
+var isMarried="No"
+console.log(isMarried)
+
