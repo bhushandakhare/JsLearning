8e5c6e8de46f6c49fa7ac947fc4c085661aa1e11
bhushandakhare <bhushandakhare20@gmail.com>
2023-09-25 11:30:22 +0530

    stringBasicAssign01
    String Basic Assignment

---
+++ jsAssignment/stringBasicsAssign01.js
@@ -0,0 +1,27 @@
+console.log("====================== Assignment stringBasicAssg01 ==========================");
+console.log("                                                                               ");
+console.log("======================= Step 1 ============================");
+function stringBasics() {
+    console.log("My dream company is: ", "Accenture");
+    
+
+console.log("                                                                               ");
+console.log("======================= Step 2 ============================");
+
+var myHobbies1 = "Playing Cricket,";
+var myHobbies2 = " Traveling,"
+var myHobbies3 = " Photography"
+
+var myhobbies =  myHobbies1 + myHobbies2 + myHobbies3;
+console.log("My Hobbies: ",myhobbies);
+
+var totalLength1= myHobbies1.length;
+console.log("Hobby1: ",totalLength1);
+var totalLength2= myHobbies2.length;
+console.log("Hobby2: ",totalLength2);
+var totalLength3= myHobbies3.length;
+console.log("Hobby3: ",totalLength3);
+}
+stringBasics();
+
+
