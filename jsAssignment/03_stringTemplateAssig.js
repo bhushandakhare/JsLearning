45e0a2c9f1a31538aab25614ea7432fede529812
bhushandakhare <bhushandakhare20@gmail.com>
2023-09-26 15:07:20 +0530

    stringTemplateAssign
    String Template Assignment

---
+++ jsAssignment/03_stringTemplateAssig.js
@@ -0,0 +1,23 @@
+console.log("====================== Assignment stringTemplate ==========================");
+console.log("                                                                               ");
+console.log("======================= Step 1 ============================");
+function stringTemplateAssig() {
+    console.log("My dream company is: ", "Accenture");
+    
+
+console.log("                                                                               ");
+console.log("======================= Step 2 ============================");
+
+var myHobbies1 = "Playing Cricket";
+var myHobbies2 = " Traveling"
+var myHobbies3 = " Photography"
+
+console.log(`"2.1 My Hobbies: ", "${myHobbies1}", "${myHobbies2}", "${myHobbies3}"`);
+
+var result=myHobbies1.concat(myHobbies2, myHobbies3);
+console.log(`2.2 String using Concat: ${result}`);
+
+}
+stringTemplateAssig();
+
+
