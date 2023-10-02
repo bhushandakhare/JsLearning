b7081e139807049098830c2e4dc6b09d0bc8cf3b
bhushandakhare <bhushandakhare20@gmail.com>
2023-10-02 18:11:14 +0530

    forAssignment
    For Loop Assignment

---
+++ jsAssignment/06_for_Assignment.js
@@ -0,0 +1,53 @@
+console.log(`=========== 5 to 15 by incrementing 1 ==========`);
+var result="";
+for (let index = 5; index <=15; index=index+1){
+result=result+index+ " ";
+   
+}
+console.log(result);
+console.log(`=========== 50 to 40 by decrementing 1 ==========`);
+var result="";
+for (let index = 50; index >=40; index--){
+result=result+index+ " ";
+   
+}
+console.log(result);
+console.log(`=========== first 15 odd numbers ==========`);
+var result="";
+for (let index = 1; index <=30; index=index+2) {
+    result= result + index + " ";
+    
+}
+console.log(result);
+
+console.log(`=========== first 10 even numbers ==========`);
+var result="";
+for (let index = 2; index <=20; index=index+2) {
+    result= result + index + " ";
+    
+}
+console.log(result);
+
+console.log(`=========== Table of 5 ==========`);
+var result="";
+for (let index = 5; index <=50; index=index+5) {
+    result= result + index + " ";
+    
+}
+console.log(result);
+
+console.log(`=========== Table of 10 ==========`);
+var result="";
+for (let index = 10; index<=100; index=index+10) {
+    result= result + index + " ";
+    
+}
+console.log(result);
+
+console.log(`=========== Table of 10 in reverse ==========`);
+var result="";
+for (let index = 100; index>=10; index=index-10) {
+    result= result + index + " ";
+    
+}
+console.log(result);
---
+++ jsAssignment/06_leapYear.js
@@ -0,0 +1,13 @@
+var leapYear=function(num){
+    
+    if (num/4) {
+        console.log(`It is a Leap Year`);
+    }
+    
+    else{
+        console.log(`Its Not A leap Year`);
+    }
+
+}
+
+leapYear(1999);
