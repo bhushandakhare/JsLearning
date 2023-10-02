7c7e52863e80ccfc5fa0789efd8817d8dd7aa9d6
bhushandakhare <bhushandakhare20@gmail.com>
2023-10-02 17:30:44 +0530

    switchAssignment
    Switch Case Month Assignment

---
+++ jsAssignment/pra.js
@@ -0,0 +1,18 @@
+
+var num = "300";
+var result = +num;
+console.log(typeof result);
+
+
+var cube= function(num){
+var result=  num * num * num;
+console.log(result);
+}
+cube(5);
+
+var str=" true";
+var result = boolean.str;
+console.log(typeof result);
+
+
+
