ec87a68da39118d6baaabb06dbfd0e23b2b5f520
bhushandakhare <bhushandakhare20@gmail.com>
2023-10-27 20:48:53 +0530

    domScriptAssignment
    Dom Script Assignment

---
+++ jsAssignment/30_scriptAssign.js
@@ -0,0 +1,10 @@
+const mainHeading = document.getElementsByTagName('h1');
+
+console.log(mainHeading[0].innerHTML);
+
+
+const secondAdvantage = document.getElementsByClassName('avail');
+console.log(secondAdvantage[0].innerHTML);
+
+const advDiv  = document.getElementById('advantages');
+console.log(advDiv.innerHTML);
---
+++ jsAssignment/31_main.js
@@ -0,0 +1,10 @@
+const heading=document.querySelector('#title');
+heading.style.color = 'purple'
+
+const listHeadingElement = document.querySelector('#listHeading');
+listHeadingElement.style.color = 'Red';
+
+
+const listElement= document.querySelector('#list');
+const liListElement= document.querySelector('.techStack');
+listElement.removeChild(liListElement)
