2870b2f5193a1a928f8416d3b264baf634e005d3
bhushandakhare <bhushandakhare20@gmail.com>
2023-10-27 20:50:27 +0530

    domAssignmentB
    Changing the color and removing Assignment

--- jsAssignment/31_main.js
+++ jsAssignment/31_main.js
@@ -4,7 +4,6 @@ heading.style.color = 'purple'
 const listHeadingElement = document.querySelector('#listHeading');
 listHeadingElement.style.color = 'Red';
 
-
 const listElement= document.querySelector('#list');
 const liListElement= document.querySelector('.techStack');
 listElement.removeChild(liListElement)
